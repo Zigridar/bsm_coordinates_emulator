8464b5778604179b8a969b5b5642bb25cbde91d1
zigridar <safonenko.max@yandex.ru>
2021-06-17 20:01:17 +0500

    mongo URI from ENV

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,15 @@
+/** get mongo url from env variable */
+const getMongoURI: () => string = () => {
+    const URI = process.env.MONGO_URI
+    if (URI)
+        return URI
+    else
+        throw new Error('mongodb URI isn`t provided!')
+}
+
 const config = {
     PORT: 8080,
-    MONGO_URI: "mongodb://192.168.43.30:27017/local"
+    MONGO_URI: getMongoURI()
 } as const
 
 export default config
